142a80b78ecc1b9f219393b1b854a54ee9116e0f
Universemul <davidbarthelemy28@gmail.com>
2020-03-03 23:16:33 +0100

    add test and cli port

--- commands.go
+++ commands.go
@@ -6,6 +6,15 @@ import (
 	"time"
 )
 
+const Port = 8001
+const Quit = "QUIT"
+const Hello = "EHLO"
+const Date = "DATE"
+const Welcome = "WELCOME"
+const DateFormat = "02-01-2006T15:04:05\n"
+
+var availableVerbs = [4]string{Quit, Hello, Date}
+
 type Command struct {
 	name string
 	args string
--- main.go
+++ main.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
+	args := os.Args[1:]
+	port := Port
+	if len(args) >= 1 {
+		if i, err := strconv.Atoi(args[0]); err == nil {
+			port = i
+		}
+	}
 	var server TcpServer
-	err := server.Listen(Port)
+	err := server.Listen(port)
 	if err != nil {
 		fmt.Println(err)
 		return
